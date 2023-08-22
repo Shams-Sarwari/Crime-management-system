ab7e8b6cd1eb683bac9e784c7f59a80b1b8164c4
Shams Sarwari <shamsullahsarwari222@gamil.com>
2023-08-22 22:42:13 +0430

    create DriverProfile, StaffProfile, Address, and Workplace databases

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import User
+from .models import User, StaffProfile, Address, WorkPlace, DriverProfile
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'licence_num', 'is_staff', 'is_driver', ]
+
+@admin.register(StaffProfile)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+
+@admin.register(DriverProfile)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'licence_num']
+
+
+admin.site.register(Address)
+admin.site.register(WorkPlace)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.hashers import make_password
+import uuid
 # Create your models here.
 class UserAccountManager(BaseUserManager):
     def create_user(self, username, email, password, licence_num=None):
@@ -45,3 +47,74 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+class StaffProfile(models.Model):
+    GENDER = (
+        ('آقا', 'آقا'),
+        ('خانم', 'خانم'), 
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    email = models.EmailField(max_length=300, unique=True, null=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    father_name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=5, choices=GENDER, default='آقا')
+    tazkira_num = models.CharField(max_length=255, unique=True, primary_key=False)
+    current_address = models.OneToOneField('Address', on_delete=models.CASCADE, null=True)
+    work_place = models.ForeignKey('WorkPlace', on_delete=models.SET_NULL, null=True)
+    phone_num = models.CharField(max_length=14,unique=True, null=True, blank=True)
+    tazkira_img = models.ImageField(upload_to="media/staff/id_images")
+    avatar = models.ImageField(upload_to="media/staff/profile_images", default="media/staff.jpg", null=True, blank=True)
+   
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    def set_password(self, raw_password):
+        if len(raw_password) < 8:
+            raise ValueError('پسورد باید حداقل دارای هشت کارکتر باشد')
+        else: 
+            self.password = make_password(raw_password)
+
+class WorkPlace(models.Model):
+    province = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.district}, {self.province}'
+
+
+
+class DriverProfile(models.Model):
+    GENDER = (
+        ('آقا', 'آقا'),
+        ('خانم', 'خانم'), 
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    licence_num = models.CharField(max_length=250, unique=True)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    father_name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=5, choices=GENDER, default='آقا')
+    blood_group = models.CharField(max_length=10, blank=True, null=True)
+    tazkira_num = models.CharField(max_length=255, unique=True, primary_key=False)
+    current_address = models.OneToOneField('Address', on_delete=models.CASCADE, blank=True, null=True)
+    phone_num = models.CharField(max_length=14,unique=True, null=True, blank=True)
+    tazkira_img = models.ImageField(upload_to="media/driver/id_images", default="media/driver_id.jpg", null=True, blank=True)
+    avatar = models.ImageField(upload_to="media/driver/profile_images", default="media/driver.jpg", null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name}, {self.last_name}, {self.license_num}'
+
+
+class Address(models.Model):
+    province = models.CharField(max_length=100,blank=True, null=True)
+    district = models.CharField(max_length=100, null=True, blank=True)
+    street = models.CharField(max_length=100,null=True, blank=True)
+    house_number = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.province}, {self.district}, {self.street}, {self.house_number}'
