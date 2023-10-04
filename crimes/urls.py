64df359c0d515ced46634fab57cf36dff680d778
Shams Sarwari <shamsullahsarwari222@gamil.com>
2023-10-04 15:55:23 +0430

    Add all CRUD operations from crime table

--- crimes/urls.py
+++ crimes/urls.py
@@ -5,5 +5,8 @@ app_name = 'crimes'
 
 urlpatterns = [
     path('create/', views.create_crime, name='create-crime'),
+    path('crime-list/', views.crime_list, name='crime-list'),
+    path('edit-crime/<int:pk>/', views.update_crime, name='edit-crime'),
+    path('delete-crime/<int:pk>/', views.delete_crime, name='delete-crime'),
 
 ]
