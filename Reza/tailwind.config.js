5bf6a554c97925b4986430387019c0d52e2b13b3
Mohammad <reza.qasimi2000@gmail.com>
2023-10-10 10:20:33 +0430

    add create driver page

--- Reza/tailwind.config.js
+++ Reza/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
         "color-primary-light": "#3c4761",
         "color-primary-lighter": "#515b72",
       },
+      fontFamily: {
+        salamat: ["A Salamat", "sans-serif"],
+        IRANSans: ["IRANSans", "sans-serif"],
+        baseet: ["Far.Baseet", "IRANSans"],
+        kamran: ["Far.Kamran", "IRANSans"],
+        roya: ["Far.Roya", "IRANSans"],
+      },
     },
   },
   plugins: [],
